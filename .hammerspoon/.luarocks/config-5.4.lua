8dd6d889100f32cfb5d765eb858dd909c8124911
Maia Yeager <42281448+maia-yeager@users.noreply.github.com>
2026-04-24 15:35:26 -0400

    fix(hammerspoon): pin lua version

---
+++ .hammerspoon/.luarocks/config-5.4.lua
@@ -0,0 +1,6 @@
+variables = {
+  LUA = "/opt/homebrew/opt/lua@5.4/bin/lua",
+  LUA_BINDIR = "/opt/homebrew/opt/lua@5.4/bin",
+  LUA_DIR = "/opt/homebrew/opt/lua@5.4",
+  LUA_INCDIR = "/opt/homebrew/opt/lua@5.4/include/lua",
+}
