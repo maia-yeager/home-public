5cf0e54a7aa5c15f6193c2a99689b1da81a4ae3f
Maia Yeager <42281448+maia-yeager@users.noreply.github.com>
2026-05-08 10:07:24 -0400

    refactor(zsh): move `ANDROID_HOME` back to `.zshrc`

--- .config/env.d/xdg.zsh
+++ .config/env.d/xdg.zsh
@@ -1,7 +1,6 @@
 # Coerce non-conformant apps to use XDG-like file structures.
 defaults write org.hammerspoon.Hammerspoon MJConfigFile ${XDG_CONFIG_HOME}/hammerspoon/init.lua
 export ANDROID_AVD_HOME=$XDG_DATA_HOME/android/avd
-export ANDROID_HOME=$HOME/Library/Android/sdk
 export ANDROID_USER_HOME=$XDG_DATA_HOME/android
 export CARGO_HOME=$XDG_DATA_HOME/cargo
 export COPILOT_HOME=$XDG_DATA_HOME/copilot
--- .config/zsh/.zshrc
+++ .config/zsh/.zshrc
@@ -156,6 +156,7 @@ zstyle ':z4h:term-title:ssh' precmd  '􀤆 %n@'${${${Z4H_SSH##*:}//\%/%%}:-%m}':
 z4h init || return
 
 # Export environment variables.
+export ANDROID_HOME=$HOME/Library/Android/sdk
 export GPG_TTY=$TTY
 export LESS='--ignore-case --quit-if-one-screen --LONG-PROMPT --RAW-CONTROL-CHARS --HILITE-UNREAD --tabs=4 --window=-4'
 export MANPAGER='less +Gg' # Show scroll progress in man pages.
